249436556b20259add41d946fc7eba8ece45d45e
keemknkx <keemknkx@gmail.com>
2022-08-11 03:01:04 +0700

    Update node.js

--- src/js/node.js
+++ src/js/node.js
@@ -3,21 +3,22 @@ am4core.ready(() => {
   const chartDiv = document.getElementById("chartdiv");
   
   am4core.useTheme(am4themes_animated);
-
+  
   var chart = am4core.create(chartDiv, "ForceDirectedTree");
   // Show data node
   // chart.legend = new am4charts.Legend();
-
+  
   // Create series
   var series = chart.series.push(
     new am4plugins_forceDirected.ForceDirectedSeries()
-  );
-
-  // Set data
-  // external load data from chart_data.json
-  series.dataSource.url = "./json/chart_data.json";
-  
+    );
+    
+    // Set data
+    // external load data from chart_data.json
+    series.dataSource.url = "./json/chart_data.json";
+    
   // Set up data fields
+  series.dataFields.id = "name";
   series.dataFields.value = "value";
   series.dataFields.name = "name";
   series.dataFields.children = "children";
@@ -27,7 +28,7 @@ am4core.ready(() => {
   // Add labels
   series.nodes.template.label.text = "{name}";
   series.fontSize = 12;
-
+  
   // lighten color children
   series.nodes.template.adapter.add("fill", function(fill, target) {
     return fill.lighten(target.dataItem.level * 0.25);
@@ -35,23 +36,32 @@ am4core.ready(() => {
 
   // Click to slide bar show course details
   // Not yet 
-  series.nodes.template.events.on("hit", (ev) => {
-    let selectedNode = ev.target;
-    if (selectedNode.isActive) {
-      console.log("selectedNode", selectedNode.isActive);
+  const sidebar = document.getElementById("mySidenav");
+  
+  const nodeCheckSidebar = (nodeCheck) => {
+    // Check sidebar is show on screen?
+    console.log(nodeCheck.isActive);
+    if (nodeCheck.isActive) {
+      console.log("nodeCheck", nodeCheck.isActive);
       console.log("Clicked");
-      // document.body.style.backgroundColor = "#fff";
-      document.getElementById("mySidenav").style.width = "0";
-
-      // selectedNode = undefined;
+      sidebar.style.width = "0";
     } else {
-      console.log("selectedNode", selectedNode.isActive);
-      if (selectedNode) {
-        // document.body.style.backgroundColor = "#1f1a24";
-        // console.log(ev.target);
-
-        document.getElementById("mySidenav").style.width = "25%";
+      console.log("nodeCheck", nodeCheck.isActive);
+      if (nodeCheck) {
+        sidebar.style.width = "25%";
       }
     }
+  }
+  
+  series.nodes.template.events.on("hit", (ev) => {
+    const selectedNode = ev.target.dataItem._dataContext.name;
+    const nodeCheck = ev.target;
+    console.log(nodeCheck);
+    // sidebar.classList.add("block");
+    console.log(selectedNode);
+    switch (selectedNode) {
+      case 'A2':
+        nodeCheckSidebar(nodeCheck);
+      }
   });
 });
